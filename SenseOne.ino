d1d26d440a5364a3cbdc4f4aa0283e5ce6966767
eagleshot <****@****.com>
2025-10-25 22:14:56 +0200

    Move modem code to seperate file

--- SenseOne.ino
+++ SenseOne.ino
@@ -11,7 +11,7 @@
 // Tools -> USB CDC on Boot: "Enabled"
 // Tools -> PSRAM: "OPI PSRAM"
 
-// /home/eagleshot_drone/uploads/image_1761387750960.jpg
+// /home/eagleshot_drone/uploads/image_1761423114435.jpg
 /*Verison*/
 //ESP32 Arduino 2.3.3
 //TinyGSM 0.12.0
@@ -33,30 +33,17 @@
 
 // Config
 #include "config.h"
+#include "time.h"
 
 // Camera
 #include "esp_camera.h"
 
-// Modem
-const char server[] = "34.65.195.210";
-const int port = 3000;
-const char resource[] = "/upload";
-
-const char apn[] = "gprs.swisscom.ch";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-#define TINY_GSM_MODEM_SIM7600
-#include <HardwareSerial.h>
-#include <TinyGsmClient.h>
-
-#define TINY_GSM_USE_GPRS false
-#define CHUNK_SIZE        1500
-
 #define uS_TO_S_FACTOR 1000000ULL
 #define TIME_TO_SLEEP  20
-TinyGsm modem(Serial1);
-TinyGsmClient client(modem, 0);
+
+const char gprsApn[] = "gprs.swisscom.ch";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
 
 void setup() {
 
@@ -64,11 +51,10 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
-  // Initialize modem power  
+  // Initialize modem  
   pinMode(PWR_ON_PIN, OUTPUT);
   pinMode(PCIE_PWR_PIN, OUTPUT);
   turn_on_modem();
-  Serial1.begin(115200, SERIAL_8N1, PCIE_RX_PIN, PCIE_TX_PIN); // Modem serial
 
   // Initialize camera and snap image
   camera_init();
@@ -78,63 +64,15 @@ void setup() {
   bool sd_card_connected = sd_begin();
 
   // Initialize modem
-  Serial.println("Initializing modem...");
-  modem.init();
+  init_modem();
+  set_network_mode();
+  print_connection_info();
+  wait_for_network();
+  modem_gprs_connect(gprsApn, gprsUser, gprsPass);
 
-  String modemInfo = modem.getModemInfo();
-  Serial.print("Modem - ");
-  Serial.println(modemInfo);
-
-  // Set Network Mode
-  // 2 Automatic , 13 GSM only , 38 LTE only , 51 GSM and LTE only
-  int res;
-  do {
-    res = modem.setNetworkMode(38);
-    Serial.print("Modem Set LTE: ");
-    Serial.println(res);
-    if (res != 1) {
-      delay(500);
-    }
-  } while (res != 1);
-
-  Serial1.println("AT+CPSI?"); // Get connection type and band
-  delay(500);
-  if (Serial1.available()) {
-    String r = Serial1.readString();
-    Serial.println(r);
-  }
-
-  Serial.print("Waiting for network...");
-  if (!modem.waitForNetwork()) {
-    Serial.println("Modem Fail");
-    delay(750);
-    return;
-  }
-  Serial.println(" success");
-
-  if (modem.isNetworkConnected()) {
-    Serial.println("Network connected");
-  }
-
-  // Connect to GPRS
-  Serial.print("Connecting to ");
-  Serial.println(apn);
-
-  const int MAX_GPRS_RETRIES = 5;
-  int attempt = 0;
-  bool gprs_ok = false;
-  while (attempt++ < MAX_GPRS_RETRIES) {
-    if (modem.gprsConnect(apn, gprsUser, gprsPass)) {
-      gprs_ok = true;
-      break;
-    }
-    Serial.println("GPRS attempt failed, retrying...");
-    delay(2000 * attempt); // simple backoff
-  }
-
-  if (gprs_ok && modem.isGprsConnected()) {
+  if (modem_is_gprs_connected()) {
     Serial.println("GPRS connected - uploading image...");
-    // uploadImage(fb);
+    uploadImage(fb);
   } else if (sd_card_connected) {
     Serial.println("GPRS not connected - saving to sd card...");
     sd_write_image("/image.jpg", fb);
--- modem.ino
+++ modem.ino
@@ -1,3 +1,19 @@
+// Modem
+const char server[] = "34.65.195.210";
+const int port = 3000;
+const char resource[] = "/upload";
+
+
+#define TINY_GSM_MODEM_SIM7600
+#include <HardwareSerial.h>
+#include <TinyGsmClient.h>
+
+#define TINY_GSM_USE_GPRS false
+#define CHUNK_SIZE        1500
+
+TinyGsm modem(Serial1);
+TinyGsmClient client(modem, 0);
+
 void turn_on_modem() {
   digitalWrite(PWR_ON_PIN, HIGH); // Turn on power to the modem
 
@@ -7,9 +23,70 @@ void turn_on_modem() {
   delay(3000);
 }
 
-void turn_off_modem() {
-  digitalWrite(PWR_ON_PIN, LOW);
+// Initialize the modem and print its information
+void init_modem() {
+  Serial.println("Initializing modem...");
+  Serial1.begin(115200, SERIAL_8N1, PCIE_RX_PIN, PCIE_TX_PIN); // Modem serial
+  modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem - ");
+  Serial.println(modemInfo);
 }
+
+// Set Network Mode
+// 2 Automatic , 13 GSM only , 38 LTE only , 51 GSM and LTE only  
+void set_network_mode() {
+  int mode = 38;
+  int res;
+  do {
+    res = modem.setNetworkMode(mode);
+    Serial.print("Modem Set LTE: ");
+    Serial.println(res);
+    if (res != 1) {
+      delay(500);
+    }
+  } while (res != 1);
+}
+
+// Get connection type and band
+void print_connection_info() {
+  Serial1.println("AT+CPSI?"); 
+  delay(500);
+  if (Serial1.available()) {
+    String r = Serial1.readString();
+    Serial.println(r);
+  }
+}
+
+void wait_for_network() {
+Serial.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    Serial.println("Modem Fail");
+    return;
+  }
+  Serial.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    Serial.println("Network connected");
+  }
+}
+
+void modem_gprs_connect(const char* apn, const char* user, const char* pass) {
+  int max_retries = 5;
+  for (int attempt = 0; attempt < max_retries; attempt++) {
+    if (modem.gprsConnect(apn, user, pass)) {
+      return;
+    }
+    Serial.println("GPRS connection failed, retrying...");
+    delay(2000 * attempt); // Simple backoff
+  }
+}
+
+int modem_is_gprs_connected(void) {
+  return modem.isGprsConnected();
+}
+
 
 void uploadImage(camera_fb_t *fb) {
   if (!client.connect(server, port)) {
@@ -69,4 +146,8 @@ void uploadImage(camera_fb_t *fb) {
   }
   Serial.println(logo);
   client.stop();
+}
+
+void turn_off_modem() {
+  digitalWrite(PWR_ON_PIN, LOW);
 }
